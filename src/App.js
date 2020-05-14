891f7dfb470672adf0f897346da6921930213246
iemprashant <prashantgupta9876@gmail.com>
2020-05-14 15:54:21 +0530

    added home component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Fragment} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/alert';
 import about from './components/pages/about';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/home';
+import NotFound from './components/pages/NotFound';
 
 const App=()=>{
-  
     return (
       <GithubState>
         <AlertState>
@@ -21,16 +20,10 @@ const App=()=>{
               <div className='container'>
                 <Alert/>
                 <Switch>
-                  <Route exact path='/'render={props => 
-                  (
-                    <Fragment>
-                      <Search/>
-                      <Users/>
-                    </Fragment>
-                  )}
-                  />
+                  <Route exact path='/'component={Home}/>
                   <Route exact path='/about' component={about} />
                   <Route exact path='/user/:login'component={User}/>
+                  <Route component={NotFound}/>
                 </Switch>
               </div>
             </div>
---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import React,{Fragment} from 'react'
+import Users from '../users/Users';
+import Search from '../users/Search';
+
+ const Home = () => {
+    return (
+    <Fragment>
+        <Search/>
+        <Users/>
+    </Fragment>
+    )
+}
+export default Home;
