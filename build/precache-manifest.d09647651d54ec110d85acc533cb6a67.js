15687f5b6a3f7cb494de55ca5d0c64ae97758449
iemprashant <prashantgupta9876@gmail.com>
2020-05-17 00:50:49 +0530

    build file

---
+++ build/precache-manifest.d09647651d54ec110d85acc533cb6a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3aa46206cd554df25046d2bb96b2672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed394f0ea5c7c4c501c",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "7799527dd3e76487d38d",
+    "url": "/static/js/2.1b1bf567.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b1bf567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed394f0ea5c7c4c501c",
+    "url": "/static/js/main.ed70bd8e.chunk.js"
+  },
+  {
+    "revision": "0d5a2a7f04463b36b262",
+    "url": "/static/js/runtime-main.877da3fe.js"
+  }
+]);
